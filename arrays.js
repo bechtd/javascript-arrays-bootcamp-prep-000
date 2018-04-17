8c66245dbb196368ddac1d7f96e527a33c14a89d
David Becht <bechtd@gmail.com>
2018-04-17 16:58:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,8 +7,8 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray(array, element) {
   var newArray new Array();
-  //newArray.push(element);
-  array = newArray + array;
+  newArray.push(element);
+  //array = newArray + array;
   //array = newArray;
   //array.insert(element, 0);
 }
